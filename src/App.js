87214e61d6c7f1b6af1452734f435b4a9cf77bf1
Neil Brown <nebrown@hof.co.uk>
2019-01-08 15:20:57 +0000

    adding UI updates

--- src/App.js
+++ src/App.js
@@ -1,65 +1,88 @@
-import React, { Component } from 'react';
-import './App.css';
-import Dice from './components/Dice';
-import Player from './components/Player';
+import React, { Component } from "react";
+import { TweenLite } from "gsap";
+import "./App.css";
+import Dice from "./components/Dice";
+import Player from "./components/Player";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       dice: [6, 6],
       players: [
-        { id: 0, name: 'Player One', score: 0 },
-        { id: 1, name: 'Player Two', score: 0 },
+        { id: 0, name: "Player One", score: 0 },
+        { id: 1, name: "Player Two", score: 0 }
       ],
       turn: 0,
       currentScore: 0,
-      gameOver: false
-    }
+      gameOver: false,
+      hideUI: false
+    };
 
-    this.rollButtonHandler = this.rollButtonHandler.bind(this)
-    this.passButtonHandler = this.passButtonHandler.bind(this)
+    this.rollButtonHandler = this.rollButtonHandler.bind(this);
+    this.passButtonHandler = this.passButtonHandler.bind(this);
   }
 
   rollButtonHandler(event) {
-    this.setState(prevState => ({
-      dice: this.getDiceValues()
-    }), () => {
-      this.updateScores()
-    })
+    this.setState(
+      prevState => ({
+        dice: this.getDiceValues()
+      }),
+      () => {
+        this.updateScores();
+      }
+    );
   }
 
   passButtonHandler(event) {
-    this.updatePlayerScore(this.state.turn, this.state.currentScore)
+    this.updatePlayerScore(this.state.turn, this.state.currentScore);
   }
 
-  updateScores() {
+  async updateScores() {
     // console.log('now update the scores for player', this.state.players[this.state.turn].name)
     switch (true) {
       case this.hasThrownMultipleOnes(): {
         // console.log('has thrown both ones', this.hasThrownMultipleOnes())
-        this.resetCurrentScore()
-        this.resetPlayerScore(this.state.turn, 0)
-        break
+        this.resetCurrentScore();
+        await this.disableUI();
+        this.resetPlayerScore(this.state.turn, 0);
+        break;
       }
 
       case this.hasThrownOne(): {
         // console.log('has thrown a one', this.hasThrownOne())
-        this.resetCurrentScore()
-        this.changePlayers()
-        break
+        this.resetCurrentScore();
+        await this.disableUI();
+        this.changePlayers();
+        break;
       }
 
       default: {
         // console.log('keep adding up for player', this.state.players[this.state.turn].name)
-        this.updateCurrentScore(this.state.dice.reduce((a, b) => a + b, 0))
+        this.updateCurrentScore(this.state.dice.reduce((a, b) => a + b, 0));
       }
     }
   }
 
   resetCurrentScore() {
-    this.setState({ currentScore: 0 });
+    this.setState({ currentScore: 0 })
+  }
+
+  disableUI() {
+    return new Promise(resolve => {
+      this.setState(
+        prevState => {
+          return { hideUI: true }
+        },
+        () => {
+          setTimeout(() => {
+            this.setState({ hideUI: false })
+            resolve("resolve")
+          }, 2000);
+        }
+      )
+    })
   }
 
   updateCurrentScore(value) {
@@ -69,50 +92,59 @@ class App extends Component {
   }
 
   hasThrownOne() {
-    return this.state.dice.some(die => die === 1)
+    return this.state.dice.some(die => die === 1);
   }
 
   hasThrownMultipleOnes() {
-    return this.state.dice.every(die => die === 1)
+    return this.state.dice.every(die => die === 1);
   }
 
   resetPlayerScore(index) {
-    const players = [...this.state.players]
-    players[index] = { ...players[index], score: 0 }
+    const players = [...this.state.players];
+    players[index] = { ...players[index], score: 0 };
 
-    this.setState({
-      players
-    }, () => {
-      this.changePlayers()
-    })
+    this.setState(
+      {
+        players
+      },
+      () => {
+        this.changePlayers();
+      }
+    );
   }
 
   updatePlayerScore(index, value) {
-    const players = [...this.state.players]
-    players[index] = { ...players[index], score: players[index].score + value }
+    const players = [...this.state.players];
+    players[index] = { ...players[index], score: players[index].score + value };
 
-    this.setState({
-      players,
-      currentScore: 0
-    }, () => {
-      if (players[index].score >= 100) {
-        this.setState({ gameOver: true });
-      } else {
-        this.changePlayers()
+    this.setState(
+      {
+        players,
+        currentScore: 0
+      },
+      () => {
+        if (players[index].score >= 100) {
+          this.setState({ gameOver: true });
+        } else {
+          this.changePlayers();
+        }
       }
-    })
+    );
   }
 
   changePlayers() {
-    this.setState(prevState => {
-      return { turn: prevState.turn === 0 ? 1 : 0 }
-    }, () => {
-      // console.log(this.state.turn)
-    });
+    this.setState(
+      prevState => {
+        return { turn: prevState.turn === 0 ? 1 : 0 };
+      },
+      () => {
+        // console.log(this.state.turn)
+      }
+    );
   }
 
   getDiceValues() {
-    return this.state.dice.map(die => Math.round(Math.random() * 5) + 1)
+    return this.state.dice.map(die => Math.round(Math.random() * 5) + 1);
   }
 
   render() {
@@ -120,26 +152,55 @@ class App extends Component {
       <div className="App">
         <header>
           <div className="players">
-            {this.state.players.map((player) =>
-              <Player key={player.id} id={player.id} name={player.name} score={player.score} active={this.state.turn === player.id} />
-            )}
+            {this.state.players.map(player => (
+              <Player
+                key={player.id}
+                id={player.id}
+                name={player.name}
+                score={player.score}
+                active={this.state.turn === player.id}
+              />
+            ))}
           </div>
-          <h3>Current score: {this.state.currentScore}</h3>
+          <h3 className="score">SCORE - {this.state.currentScore}</h3>
         </header>
         <section>
-          {!this.state.gameOver &&
-            <section>
-              <button data-test="test-roll-button" className="button" type="button" onClick={this.rollButtonHandler}>Roll</button>
-              <button data-test="test-pass-button" className="button" type="button" onClick={this.passButtonHandler}>Pass</button>
+          {!this.state.gameOver && !this.state.hideUI && (
+            <section className="panel">
+              <button
+                data-test="test-roll-button"
+                className="button"
+                type="button"
+                onClick={this.rollButtonHandler}
+              >
+                Roll
+              </button>
+              <button
+                data-test="test-pass-button"
+                className="button"
+                type="button"
+                onClick={this.passButtonHandler}
+              >
+                Pass
+              </button>
             </section>
-          }
-          {this.state.gameOver &&
-            <p className="status">The winner is {this.state.players[this.state.turn].name}</p>
-          }
+          )}
+          {!this.state.gameOver && this.state.hideUI && (
+            <section className="panel">
+              <p className="status">
+                UNLUCKY {this.state.players[this.state.turn].name}
+              </p>
+            </section>
+          )}
+          {this.state.gameOver && (
+            <p className="status">
+              The winner is {this.state.players[this.state.turn].name}
+            </p>
+          )}
           <section className="dice">
-            {this.state.dice.map((die, i) =>
+            {this.state.dice.map((die, i) => (
               <Dice value={die} key={i} />
-            )}
+            ))}
           </section>
         </section>
       </div>
--- src/components/Dice.js
+++ src/components/Dice.js
@@ -1,22 +1,28 @@
 import React from "react";
-import { ReactComponent as One} from "../icons/1.svg";
-import { ReactComponent as Two} from "../icons/2.svg";
-import { ReactComponent as Three} from "../icons/3.svg";
-import { ReactComponent as Four} from "../icons/4.svg";
-import { ReactComponent as Five} from "../icons/5.svg";
-import { ReactComponent as Six} from "../icons/6.svg";
+import { ReactComponent as One } from "../icons/1.svg";
+import { ReactComponent as Two } from "../icons/2.svg";
+import { ReactComponent as Three } from "../icons/3.svg";
+import { ReactComponent as Four } from "../icons/4.svg";
+import { ReactComponent as Five } from "../icons/5.svg";
+import { ReactComponent as Six } from "../icons/6.svg";
 
-const Dice = ({ value }) => {
-  return (
-    <React.Fragment>
-      {value === 1 && <One />}
-      {value === 2 && <Two />}
-      {value === 3 && <Three />}
-      {value === 4 && <Four />}
-      {value === 5 && <Five />}
-      {value === 6 && <Six />}
-    </React.Fragment>
-  );
-};
+class Dice extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        {this.props.value === 1 && <One className="warning" />}
+        {this.props.value === 2 && <Two />}
+        {this.props.value === 3 && <Three />}
+        {this.props.value === 4 && <Four />}
+        {this.props.value === 5 && <Five />}
+        {this.props.value === 6 && <Six />}
+      </React.Fragment>
+    );
+  }
+}
 
 export default Dice;
